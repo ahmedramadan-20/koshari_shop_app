442930fd2991c3dff1f934d1e76e15572ab07f09
ahmed ramadan <ahmed.ramadan20@outlook.com>
2023-09-24 22:16:39 +0300

    added models and fonts

---
+++ lib/models/koshary_model.dart
@@ -0,0 +1,13 @@
+class Koshary {
+  String name;
+  String size;
+  String price;
+  String image;
+
+  Koshary({
+    required this.name,
+    required this.size,
+    required this.price,
+    required this.image,
+  });
+}
---
+++ lib/models/models.dart
@@ -0,0 +1,2 @@
+export 'koshary_model.dart';
+export 'user_status.dart';
